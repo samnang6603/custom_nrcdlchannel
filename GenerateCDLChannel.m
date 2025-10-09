03f972528d2ad89ef1e40b0be900d8ee6abb1088
samnang6603 <ansamnangz@gmail.com>
2025-10-09 14:33:43 -0700

    small updates

--- GenerateCDLChannel.m
+++ GenerateCDLChannel.m
@@ -1,34 +1,51 @@
 function [H,sampleTimes,cdl_struct] = GenerateCDLChannel(cdl_struct)
-% Generate NR CDL Channel, 
-% inspired by MATLAB nrCDLChannel()
+% Generates 5G NR Clustered Delay Line (CDL) Channel. CDL simulates how 
+% radio signals propagate, particularly when the received signal is 
+% composed of multiple delayed "clusters" of multipath components. 
+% Each cluster has a distinct delay, but within a cluster, the many 
+% individual paths have slightly different angles of departure and arrival, 
+% representing reflections and scattering from different objects in the 
+% environment.
 
-% Step 1: configure antenna structure
+% Input:
+% - cdl_struct: Structure containing CDL Paramaters needed to construct the
+% channel
+%
+% Output:
+% - H: Channel impulse response
+% - sampleTimes: Sample times for channel filtering operatioons
+% - cdl_struct: includes new parameters based on unique channel
+% characteristics and computations
+
+% Inspired by MATLAB nrCDLChannel()
+
+%% Step 1: configure antenna structure
 ant_struct = AntennaStructure.ConfigureStructure(cdl_struct);
 
-% Step 2: configure delay profile
+%% Step 2: configure delay profile
 pdp_struct = PowerDelayProfile.CreatePowerDelayProfileStructure(cdl_struct);
 cdl_struct.PDP = pdp_struct;
 
-% Step 3: split LOS an NLOS cluster and sub-clustering
+%% Step 3: split LOS an NLOS cluster and sub-clustering
 [pdp_struct,info_struct] = PowerDelayProfile.GetClusterInfo(pdp_struct);
 cdl_struct.PDPInfo = info_struct;
 
-% Step 4: generate initial phase
+%% Step 4: generate initial phase
 [Phi,cdl_struct] = ChannelPhase.GenerateInitialPhases( ...
     cdl_struct,ant_struct,info_struct);
 
-% Step 5: compute ray coupling
+%% Step 5: compute ray coupling
 coupling = RayModel.ComputeRayCoupling(cdl_struct,info_struct);
 
-% Step 6: calcualte dual mobility scatterer variables
+%% Step 6: calcualte dual mobility scatterer variables
 [scattererStates,scattererSpeeds] = DualMobility.ComputeDualMobilityScattererVariables( ...
     cdl_struct,info_struct);
 
-% Step 7: generate static CDL Channel
+%% Step 7: generate static CDL Channel
 static_struct = ChannelCoefficient.GenerateStaticCDLChannel( ...
     cdl_struct,pdp_struct,ant_struct,info_struct,coupling,Phi);
 
-% Step 8: generate time varying CDL Channel
+%% Step 8: generate time varying CDL Channel
 [H,sampleTimes] = ChannelCoefficient.GenerateTimeVaryingCDLChannel(cdl_struct,...
     ant_struct,static_struct,scattererStates,scattererSpeeds);
 
